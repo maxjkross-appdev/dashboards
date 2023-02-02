e7dcc6e3779072df39fa72c1d26a2c7e03c1c04b
maxjkross <maxjkross@uchicago.edu>
2023-02-02 06:52:44 +0000

    newest commit

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,34 @@
+class CurrenciesController < ApplicationController
+  def first_currency
+
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+
+    @array_of_symbols = @symbols_hash.keys
+    render({:template => "currency_templates/step_one.html.erb"})
+  end
+
+  def second_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols = @symbols_hash.keys
+    @from_symbol = params.fetch("from_currency")
+    render({:template => "currency_templates/step_two.html.erb"})
+  end
+
+  def conversion
+    @from_symbol = params.fetch("from_currency").to_sym
+    @to_symbol = params.fetch("to_currency").to_sym
+    @raw_data = open("https://api.exchangerate.host/convert?from=#{@from_symbol}to#{@to_symbol}").read
+    @parsed_data = JSON.parse(@raw_data)
+    @hash = @parsed_data.fetch("info")
+    @rate = @hash.fetch("rate")
+    
+    render({:template => "currency_templates/step_three.html.erb"})
+  end
+
+end
